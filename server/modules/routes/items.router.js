9f7f5b348bf60084e0654f0d5b5d69a842985c91
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2020-02-14 15:20:52 -0600

    POST working

--- server/modules/routes/items.router.js
+++ server/modules/routes/items.router.js
@@ -16,7 +16,14 @@ router.get( '/', ( req, res )=>{
 
 router.post( '/', (req, res )=>{
     console.log( 'in /items POST:', req.body );
-    res.send( 'meow' ); 
-})
+    let queryString = `INSERT INTO items ( "size", "color", "description" )
+        VALUES ( $1, $2, $3)`;
+    pool.query( queryString, [ req.body.size, req.body.color, req.body.description ]).then( ( results )=>{
+        res.sendStatus( 201 );
+    }).catch( ( err )=>{
+        console.log( err );
+        res.sendStatus( 500 );
+    }) // end query
+}) // end POST 
 // exports
 module.exports = router;
